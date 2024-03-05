a11fa0b3dacd6910381b596f19c4bd1c1d184231
Taimoor Aleem <taimooraleem01@gmail.com>
2024-03-05 01:51:32 -0500

    Create Welcome Page

--- dollarsenseapp-client/src/App.tsx
+++ dollarsenseapp-client/src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from './components/ui/button';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex flex-col h-screen  items-center justify-center">
+      <h1 className="text-2xl font-bold mb-6">Welcome to</h1>
+      <h1 className="text-7xl font-bold mb-10">Dollar Sense</h1>
+      <div className="space-x-2">
+        <Button className="font-semibold py-2 px-4 border-orange-600 hover:bg-orange-600 shadow-orange" variant={'outline'}>
+          Login
+        </Button>
+        <Button className="font-semibold py-2 px-4 border-orange-600 hover:bg-orange-600 shadow-orange" variant={'outline'}>
+          Register
+        </Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
