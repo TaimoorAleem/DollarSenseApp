115d3b8246d5cce65d8ccee2e330784852799603
Taimoor Aleem <taimooraleem01@gmail.com>
2024-03-17 15:17:48 -0400

    Add Sparkles component in Welcome Page.

--- dollarsenseapp-client/src/pages/WelcomePage.tsx
+++ dollarsenseapp-client/src/pages/WelcomePage.tsx
@@ -1,22 +1,32 @@
 import { Link } from 'react-router-dom';
 import { Button } from '../components/ui/button';
+import { SparklesCore } from '@/components/ui/sparkles';
 
 const WelcomePage = () => {
   return (
     <div className="flex flex-col h-screen  items-center justify-center">
-      <h1 className="text-2xl font-bold mb-6">Welcome to</h1>
-      <h1 className="text-7xl font-bold mb-10 text-primary">Dollar Sense</h1>
-      <div className="space-x-2">
-        <Link to="/dashboard">
-          <Button className="font-semibold py-2 px-4 border-orange-600 hover:bg-orange-600 shadow-orange" variant={'outline'}>
-            Login
-          </Button>
-        </Link>
-        <Link to="/register">
-          <Button className="font-semibold py-2 px-4 border-orange-600 hover:bg-orange-600 shadow-orange" variant={'outline'}>
-            Register
-          </Button>
-        </Link>
+      <h1 className="md:text-7xl text-3xl lg:text-7xl font-bold text-center text-primary relative z-20">
+        Dollar Sense
+      </h1>
+      <div className="w-full h-50 relative">
+        {/* Gradients */}
+        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-orange-500 to-transparent h-[2px] w-3/4 blur-sm" />
+        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-orange-500 to-transparent h-px w-3/4" />
+        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-orange-700 to-transparent h-[5px] w-1/4 blur-sm" />
+        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-orange-700 to-transparent h-px w-1/4" />
+ 
+        {/* Core component */}
+        <SparklesCore
+          background="transparent"
+          minSize={0.4}
+          maxSize={1}
+          particleDensity={1200}
+          className="w-full h-full"
+          particleColor="#FFA500"
+        />
+ 
+        {/* Radial Gradient to prevent sharp edges */}
+        <div className="absolute inset-0 w-full h-full bg-background [mask-image:radial-gradient(500px_200px_at_top,transparent_10%,#FFA500)]"></div>
       </div>
     </div>
   )
