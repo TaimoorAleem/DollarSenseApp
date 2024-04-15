3929876710ced711dbfca48635a0b349cfe05267
Taimoor Aleem <taimooraleem01@gmail.com>
2024-04-14 23:31:48 -0400

    Add CSV File Upload button in NextMonthPredictions table

--- dollarsenseapp-client/src/components/RecentTransactions.tsx
+++ dollarsenseapp-client/src/components/RecentTransactions.tsx
@@ -6,79 +6,14 @@ import {
     TableHeader,
     TableRow,
   } from "@/components/ui/table"
-
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
-
-import { Button } from "./ui/button"
-import { Upload } from "lucide-react"
-import axios from "axios"
   
 
 const RecentTransactions = () => {
-  const handleFileUpload = (event) => {
-    event.preventDefault();
-    const formData = new FormData();
-    const fileField = document.querySelector('input[type="file"]');
-  
-    // Append the file to the FormData instance
-    formData.append('file', fileField.files[0]);
-  
-    // Use axios to send a post request
-    axios.post('http://127.0.0.1:5000/predict', formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data'
-      }
-    })
-    .then(response => {
-      // Handle response data
-      console.log(response.data);
-    })
-    .catch(error => {
-      console.error("Error uploading the file", error);
-    });
-  };
   
   return (
     <div>
         <div className="flex flex-row justify-between align-middle items-center m-2 h-10">
           <h2>Your Recent Transactions</h2>
-            <Dialog>
-              <DialogTrigger asChild>
-                <Button variant="outline"><Upload className="mr-2 h-4 w-4" />Upload CSV</Button>
-              </DialogTrigger>
-              <DialogContent className="sm:max-w-[425px]">
-                <DialogHeader>
-                  <DialogTitle>Upload CSV File</DialogTitle>
-                  <DialogDescription>
-                    Please upload a CSV file of your most recent transactions
-                  </DialogDescription>
-                </DialogHeader>
-                <form onSubmit={handleFileUpload}>
-                  <div className="grid gap-4 py-4">
-                    <input
-                      type="file"
-                      accept=".csv"
-                      name="file"
-                    />
-                  </div>
-                  <DialogFooter>
-                    <Button type="submit">Upload</Button>
-                    <DialogClose asChild>
-                      <Button variant="outline">Cancel</Button>
-                    </DialogClose>
-                  </DialogFooter>
-                </form>
-              </DialogContent>
-            </Dialog>
         </div>
         <Table className="bg-secondary rounded-xl p-4 mt-2">
           <TableHeader>
